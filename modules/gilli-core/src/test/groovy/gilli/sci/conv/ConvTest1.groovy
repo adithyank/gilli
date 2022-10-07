1a7aca1b08cf93195d422582d260d09d6cfa5b03
K Adithyan <adi.k.dev@gmail.com>
2022-10-07 10:59:06 +0530

    multiple modules removed and retained only `core` and `cover` module

---
+++ modules/gilli-core/src/test/groovy/gilli/sci/conv/ConvTest1.groovy
@@ -0,0 +1,11 @@
+package gilli.sci.conv
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+@Test
+void test2()
+{
+    //Assert.assertEquals(convert.length(1000).meter.toMile.valueWithUnit, '0.6213712000 mile')
+}
+
