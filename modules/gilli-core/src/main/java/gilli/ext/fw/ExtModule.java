6bd384eb7283961e0efb8a20018d7c34759e2e3b
K Adithyan <adi.k.dev@gmail.com>
2023-01-06 12:52:18 +0530

    comment added

--- modules/gilli-core/src/main/java/gilli/ext/fw/ExtModule.java
+++ modules/gilli-core/src/main/java/gilli/ext/fw/ExtModule.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * APT Not yet tested
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ExtModule
