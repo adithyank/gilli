f824ae0d6be31af00694d0d3d77f5fcd381291c2
K Adithyan <adi.k.dev@gmail.com>
2023-09-28 15:20:26 +0530

    unwanted file removed

--- modules/gilli-core/src/main/groovy/gilli/extras/numerology/Numerology.java
+++
@@ -1,9 +0,0 @@
-package gilli.extras.numerology;
-
-import picocli.CommandLine;
-
-@CommandLine.Command(name = "numerology", sortOptions = false, synopsisHeading = "Line Diff as Set of Lines. So, lines order does not matter\n\n", description = "Line Diff as Set of Lines. So, lines order does not matter")
-public class Numerology
-{
-
-}
