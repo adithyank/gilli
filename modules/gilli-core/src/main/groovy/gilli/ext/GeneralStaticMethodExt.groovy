e049e8ef401c9828a498ba3bdaeaad51c426cfeb
K Adithyan <adi.k.dev@gmail.com>
2023-09-28 15:20:18 +0530

    GeneralStaticMethodExt.log method added

--- modules/gilli-core/src/main/groovy/gilli/ext/GeneralStaticMethodExt.groovy
+++ modules/gilli-core/src/main/groovy/gilli/ext/GeneralStaticMethodExt.groovy
@@ -8,4 +8,9 @@ class GeneralStaticMethodExt
     {
         return GeneralUtil.randomWithin(bound);
     }
+
+    static double log(Math m, Number base, Number value)
+    {
+        return Math.log(value.doubleValue()) / Math.log(base.doubleValue());
+    }
 }
